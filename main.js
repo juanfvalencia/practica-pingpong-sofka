2fc5f68f4dab95f4c2863aa359e176633ad8da50
juanfvalencia <90592176+juanfvalencia@users.noreply.github.com>
2021-11-20 13:56:51 -0500

    Se crea la funcion pelota

--- main.js
+++ main.js
@@ -18,3 +18,52 @@
       },
     };
   })();
+
+  (function pelota() {
+    self.Ball=function(x,y,radius,board) {
+        this.x=x;
+        this.y=y;
+        this.radius=radius;
+        this.board=board;
+        this.speed_y=0;
+        this.speed_x=5;
+        this.board.ball=this;
+        this.kind="circle"
+        this.direccion=1;
+        this.speed=5;
+        this.bounce_angle=0;
+        this.max_bounce_angle=Math.PI/12;
+    }
+    self.Ball.prototype={
+        move: function(){
+            this.x+=(this.speed_x * this.direccion );
+            this.y+=(this.speed_y);
+        },
+        collision: function (bar) {
+            var relative_intersect_y= (bar.y +(bar.height/2))-this.y;
+            var normalized_intersect_y=relative_intersect_y / (bar.height/2);
+            this.bounce_angle=normalized_intersect_y*this.max_bounce_angle;
+            this.speed_y=this.speed *-Math.sin(this.bounce_angle);
+            this.speed_x=this.speed * Math.cos(this.bounce_angle);
+
+            if(this.x>(this.board.width/2)) this.direccion=-1;
+            else this.direccion=1;
+        },
+        collisionBorder: function () {
+            if(this.y<=100){
+            this.speed_y=this.speed*Math.sin(this.bounce_angle)
+            this.speed_x=this.speed}
+            else{
+                this.speed_y=-this.speed*(Math.random()*(0.5-0.1)+0.1);
+                this.speed_x=this.speed}
+            
+        },
+        get width(){
+            return this.radius*2;
+        },
+        get height(){
+            return this.radius*2;
+        }
+    }
+
+})();
